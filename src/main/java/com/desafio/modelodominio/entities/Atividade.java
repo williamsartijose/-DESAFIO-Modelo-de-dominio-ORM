2b7392a3f10526fc138c984630771150100c3081
unknown <williamsartijose@hotmail.com>
2023-01-20 22:39:45 +0000

    Criando a classe Entities - Atividade e Criando as configuração do banco de dados H2

---
+++ src/main/java/com/desafio/modelodominio/entities/Atividade.java
@@ -0,0 +1,67 @@
+package com.desafio.modelodominio.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private Double preco;
+	
+	public Atividade() {
+		
+		
+	}
+
+	public Atividade(Long id, String nome, String descricao, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
