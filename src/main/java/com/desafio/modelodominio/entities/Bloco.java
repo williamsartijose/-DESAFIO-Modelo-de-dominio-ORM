ecaf120b5d3a0134244bae9217faab04f1ef5e81
unknown <williamsartijose@hotmail.com>
2023-01-20 22:45:39 +0000

    Criando a classe Entities - Bloco

---
+++ src/main/java/com/desafio/modelodominio/entities/Bloco.java
@@ -0,0 +1,67 @@
+package com.desafio.modelodominio.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Instant inicio;
+	
+	private Instant fim;
+	
+	
+	public Bloco() {
+		
+	}
+
+
+	public Bloco(Long id, Instant inicio, Instant fim) {
+		super();
+		this.id = id;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Instant getInicio() {
+		return inicio;
+	}
+
+
+	public void setInicio(Instant inicio) {
+		this.inicio = inicio;
+	}
+
+
+	public Instant getFim() {
+		return fim;
+	}
+
+
+	public void setFim(Instant fim) {
+		this.fim = fim;
+	}
+
+	
+}
