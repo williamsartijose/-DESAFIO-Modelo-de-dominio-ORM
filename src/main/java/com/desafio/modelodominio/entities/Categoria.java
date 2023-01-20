d5678e7279fa8be061495c796798ce1aadb2b257
unknown <williamsartijose@hotmail.com>
2023-01-20 22:43:10 +0000

    Criando a classe Entities - Categoria

---
+++ src/main/java/com/desafio/modelodominio/entities/Categoria.java
@@ -0,0 +1,55 @@
+package com.desafio.modelodominio.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;
+	
+	
+	public Categoria() {
+		
+	}
+
+
+	public Categoria(Long id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+
+}
