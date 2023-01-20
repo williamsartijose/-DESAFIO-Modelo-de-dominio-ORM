6208f3fb3a7289b46facc2e228a9b5955b650815
unknown <williamsartijose@hotmail.com>
2023-01-20 22:26:16 +0000

    Criando a classe Entities - Participante

---
+++ src/main/java/com/desafio/modelodominio/entities/Participante.java
@@ -0,0 +1,61 @@
+package com.desafio.modelodominio.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Column(unique = true)
+	private String email;
+	
+	public  Participante(){
+		
+	}
+
+	public Participante(Long id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
